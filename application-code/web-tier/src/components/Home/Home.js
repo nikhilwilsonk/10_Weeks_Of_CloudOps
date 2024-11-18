01143daaef6b97aec59ed236121971eb5f34a915
nikhilss219 <nikhil.wilsonk@gmail.com>
2024-11-18 14:15:22 +0530

    Changed arch diagram and Home.js

--- application-code/web-tier/src/components/Home/Home.js
+++ application-code/web-tier/src/components/Home/Home.js
@@ -1,12 +1,12 @@
 
     import React, {Component} from 'react';
-    import architecture from '../../assets/3TierArch.png'
+    import architecture from '../../assets/WebApp3Tier.png'
 
     class Home extends Component {
         render () {
         return (
             <div>
-            <h1 style={{color:"white"}}>AZURE 3-TIER WEB APP DEMO</h1>
+            <h1 style={{color:"white"}}>AZURE WEB APP 3 Tier Architecture</h1>
             <img src={architecture} alt="3T Web App Architecture" style={{height:400,width:825}} />
           </div>
         );
